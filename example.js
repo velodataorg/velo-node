0b747f45e671e1c7478135d956b9be07f1da246c
rhodey <rhodey@anhonesteffort.org>
2023-10-02 14:46:28 -0400

    init

---
+++ example.js
@@ -0,0 +1,45 @@
+const velo = require('velo-node')
+
+async function doSomethingWith(row) {
+  // todo: your logic
+  console.log(row.exchange, row.coin, row.product, row.time, row.open_price, row.close_price)
+}
+
+async function getAllFutures() {
+  const allFutures = await client.futures()
+  /*
+  const allSpot = await client.spot()
+  const allOptions = await client.options()
+  */
+  return allFutures
+}
+
+async function getRandomFuture() {
+  const allFutures = await getAllFutures()
+  let random = Math.floor(Math.random() * allFutures.length)
+  random = allFutures[random]
+  console.log('random future', random.exchange, random.coin, random.product, random.begin)
+  return random
+}
+
+async function doWork() {
+  const random = await getRandomFuture()
+  const params = {
+    type: 'futures', // futures, spot, or options
+    columns: ['open_price', 'close_price'],
+    exchanges: [random.exchange],
+    products: [random.product],
+    begin: Date.now() - (1000 * 60 * 11), // 10 minutes
+    end: Date.now(),
+    resolution: 1 // 1 minute
+  }
+
+  const rows = client.rows(params)
+  for await (const row of rows) {
+    await doSomethingWith(row)
+  }
+}
+
+const client = new velo.Client('your_api_key')
+
+doWork().catch(console.error)
---
+++ index.js
@@ -0,0 +1,3 @@
+const Client = require('./lib/client.js')
+
+module.exports = { Client }
