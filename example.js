34489ca79211e4bae171c5da0f0b9beee3b2f214
rhodey <rhodey@anhonesteffort.org>
2023-10-04 18:09:28 -0400

    version 1.1.0

--- example.js
+++ example.js
@@ -3,8 +3,8 @@ const velo = require('velo-node')
 async function doWork() {
   const futures = await client.futures()
   const future = futures[0]
-  
-  const columns = await client.futures_columns()
+
+  const columns = await client.futuresColumns()
   const twoColumns = columns.slice(0, 2)
 
   const params = {
@@ -12,9 +12,9 @@ async function doWork() {
     columns: twoColumns,
     exchanges: [future.exchange],
     products: [future.product],
-    begin: Date.now() - 1000 * 60 * 11,
+    begin: Date.now() - 1000 * 60 * 11, // 10 minutes
     end: Date.now(),
-    resolution: 1
+    resolution: 1 // 1 minute
   }
 
   const rows = client.rows(params)
